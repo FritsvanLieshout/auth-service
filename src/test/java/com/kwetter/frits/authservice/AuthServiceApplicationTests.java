5318eb6a0b016f08042a5cb775be8f8a48e87c9b
Frits van Lieshout <fritske05@gmail.com>
2021-04-10 16:42:12 +0200

    Initial Commit

---
+++ src/test/java/com/kwetter/frits/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwetter.frits.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
